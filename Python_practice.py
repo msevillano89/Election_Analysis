86a4c47eb4ff0f8861bd09227e3141b010c8a088
msevillano89 <104663655+msevillano89@users.noreply.github.com>
2022-05-30 15:40:04 -0500

    Delete Python_practice.py

--- Python_practice.py
+++
@@ -1,8 +0,0 @@
-#The data we need to retrieve
-import csv
-dir(csv)
-#1. The total number of votes cast
-#2. A complete list of candidates who received votes
-#3. The percentage of votes each candidate won
-#4. The total number of votes each candidate won
-#5. The winner of the election based on popular vote
